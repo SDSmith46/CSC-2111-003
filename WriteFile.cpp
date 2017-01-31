19b8c29c2fd8fb6aa5b08e8486c09985566bae3f
Areeb Khan <aakhan43@students.tntech.edu>
2017-01-30 19:18:39 -0600

    program

--- WriteFile.cpp
+++ WriteFile.cpp
@@ -1,15 +1,26 @@
 #include "WriteFile.h"
 #include <sstream>
+<<<<<<< HEAD
+
+=======
 using namespace std;
+>>>>>>> 60fd7b6a6401fe9f2b7e14c641fb75fbc675fe3f
 WriteFile::WriteFile(const char* file_name)
 {
    output_file.open(file_name);
    closed = false;
 }
 
+<<<<<<< HEAD
+WriteFile::WriteFile()
+{
+   close();
+   delete this;
+=======
 WriteFile::~WriteFile()
 {
    close();
+>>>>>>> 60fd7b6a6401fe9f2b7e14c641fb75fbc675fe3f
 }
 
 void WriteFile::close()
@@ -25,6 +36,10 @@ void WriteFile::writeLine(String* line)
 {
    if (!closed && line->length() > 0)
    {
+<<<<<<< HEAD
+     output_file << line->getText() << endl;
+=======
       output_file << line->getText() << endl;
+>>>>>>> 60fd7b6a6401fe9f2b7e14c641fb75fbc675fe3f
    }
 }
--- WriteFile.h
+++ WriteFile.h
@@ -6,11 +6,19 @@
 #include <fstream>
 using namespace std;
 
-struct WriteFile
+class WriteFile
 {
 	private:
 		ofstream output_file;
 		bool closed;
+<<<<<<< HEAD
+	
+	public:
+		WriteFile(const char* file_name);
+		~WriteFile();
+		void close();
+		void writeLine(String* line);
+=======
 		
 	public:
 		WriteFile(const char* file_name);
@@ -18,6 +26,7 @@ struct WriteFile
 		void writeLine(String* line);
 		void close();
 		
+>>>>>>> 60fd7b6a6401fe9f2b7e14c641fb75fbc675fe3f
 };
 
 #endif
